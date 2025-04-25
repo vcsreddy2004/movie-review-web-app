16c5211a3f83fd11bcefc47a28fa08a977dd8ab2
= <=>
2025-04-25 18:47:10 +0530

    added model

--- moviereviews/main/views.py
+++ moviereviews/main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from main.models import Movie,Review
 import pickle as pkl
+import os
+from django.conf import settings
 def home(req):
     moviesData = Movie.objects.all()
     return render(req,"index.html",{"movies":moviesData})
@@ -14,7 +16,8 @@ def movieViewPage(req,movieId):
     reviews = Review.objects.filter(movieId=movieId)
     return render(req,"movie-view-page.html",{"movieData":movieData,"reviews":reviews})
 def predict(text):
-    with open("./../models/movie_reviews.pkl", "rb") as file:
+    path = os.path.join(settings.BASE_DIR,"models","movie_reviews.pkl")
+    with open(path, "rb") as file:
         model = pkl.load(file)
-    output = ["nagative","positive"]
+    output = ["negative","positive"]
     return output[model.predict([text])[0]]
